86ee4fad857b159d364bbef31bb3c1e358f1392c
Aaron Gundel <aaron.gundel@gmail.com>
2025-02-28 13:25:13 -0700

    fixes issues with initial values

--- arches_component_lab/views/api/relatable_resources.py
+++ arches_component_lab/views/api/relatable_resources.py
@@ -7,6 +7,7 @@ from arches.app.utils.response import JSONResponse
 from arches.app.utils.betterJSONSerializer import JSONDeserializer
 
 from arches.app.utils.decorators import user_can_read_resource
+from django.db.models import Value, Case, When, IntegerField
 
 
 class RelatableResourcesView(View):
@@ -19,7 +20,7 @@ class RelatableResourcesView(View):
         )
         page_number = request.GET.get("page", 1)
         items_per_page = request.GET.get("items", 25)
-
+        initial_values = request.GET.getlist("initialValue", "")
         config = JSONDeserializer().deserialize(node.config.value)
         graphs = [graph["graphid"] for graph in config.get("graphs", [])]
 
@@ -27,17 +28,32 @@ class RelatableResourcesView(View):
             "name", "graphid"
         )
 
-        resources = ResourceInstance.objects.filter(graph_id__in=graphs).order_by(
-            "descriptors__{}__name".format(get_language())
+        resources = ResourceInstance.objects.filter(graph_id__in=graphs).annotate(
+            order_field=Case(
+                When(resourceinstanceid__in=initial_values, then=Value(0)),
+                default=Value(1),
+            )
         )
 
-        paginator = Paginator(resources, items_per_page)
+        initial_resource_count = len(initial_values)
+        items_per_page = (
+            initial_resource_count
+            if int(initial_resource_count) > int(items_per_page)
+            else items_per_page
+        )
+        paginator = Paginator(
+            resources.order_by(
+                "order_field", "descriptors__{}__name".format(get_language())
+            ),
+            items_per_page,
+        )
         page_object = paginator.get_page(page_number)
         language = get_language()
         data = [
             {
                 "resourceinstanceid": resource.resourceinstanceid,
                 "display_value": resource.descriptors[language]["name"],
+                "order_field": resource.order_field,
             }
             for resource in page_object
         ]
