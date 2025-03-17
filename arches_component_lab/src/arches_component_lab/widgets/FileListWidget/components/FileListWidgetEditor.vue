0970bda58b543ce47f0e524caaaee587b8e48f70
Aaron Gundel <aaron.gundel@gmail.com>
2025-03-17 10:08:16 -0600

    adds file list widget viewer, stub for editor

---
+++ arches_component_lab/src/arches_component_lab/widgets/FileListWidget/components/FileListWidgetEditor.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { useTemplateRef, watch } from "vue";
+import dayjs from "dayjs";
+
+import DatePicker from "primevue/datepicker";
+import Message from "primevue/message";
+
+import { FormField } from "@primevue/forms";
+import type { FormFieldResolverOptions } from "@primevue/forms";
+
+const props = defineProps<{
+    initialValue: string | null | undefined;
+    graphSlug: string;
+    nodeAlias: string;
+    widgetData: {
+        config: {
+            dateFormat: string;
+            datePickerDisplayConfiguration: {
+                dateFormat: string;
+                shouldShowTime: boolean;
+            };
+        };
+    };
+}>();
+
+const formFieldRef = useTemplateRef("formFieldRef");
+
+// this watcher is necessary to be able to format the value of the form field when the date picker is updated
+watch(
+    // @ts-expect-error - This is a bug in the PrimeVue types
+    () => formFieldRef.value?.field?.states?.value,
+    (newVal, oldVal) => {
+        if (newVal !== oldVal) {
+            // @ts-expect-error - This is a bug in the PrimeVue types
+            formFieldRef.value!.field.states.value = dayjs(newVal).format(
+                props.widgetData.config.dateFormat,
+            );
+        }
+    },
+);
+
+// let timeout: ReturnType<typeof setTimeout>;
+
+function resolver(e: FormFieldResolverOptions) {
+    validate(e);
+    // return new Promise((resolve) => {
+    //     if (timeout) clearTimeout(timeout);
+
+    //     timeout = setTimeout(() => {
+    //         resolve(validate(e));
+    //     }, 500);
+    // });
+}
+
+function validate(e: FormFieldResolverOptions) {
+    console.log("validate", e);
+    // API call to validate the input
+    // if (true) {
+    //     return {};
+    // } else {
+    //     return {
+    //         errors: [
+    //             { message: "This is an error message" },
+    //             { message: "This is also an error message" },
+    //         ],
+    //     };
+    // }
+}
+</script>
+
+<template>
+    <FormField
+        ref="formFieldRef"
+        v-slot="$field"
+        :name="props.nodeAlias"
+        :initial-value="props.initialValue"
+        :resolver="resolver"
+    >
+        <DatePicker
+            icon-display="input"
+            :date-format="
+                props.widgetData.config.datePickerDisplayConfiguration
+                    .dateFormat
+            "
+            :fluid="true"
+            :show-icon="true"
+            :show-time="
+                props.widgetData.config.datePickerDisplayConfiguration
+                    .shouldShowTime
+            "
+            :show-seconds="
+                props.widgetData.config.datePickerDisplayConfiguration
+                    .shouldShowTime
+            "
+            @keydown.enter.prevent
+        />
+        <Message
+            v-for="error in $field.errors"
+            :key="error.message"
+            severity="error"
+            size="small"
+        >
+            {{ error.message }}
+        </Message>
+    </FormField>
+</template>
