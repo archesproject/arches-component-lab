daf13f005a3635106777a4d6c00c980bc0f524fc
Jacob Walls <jwalls@fargeo.com>
2025-05-16 10:37:02 -0400

    Simplify node and widget views

--- arches_component_lab/views/api/widgets.py
+++ arches_component_lab/views/api/widgets.py
@@ -1,27 +1,10 @@
-from arches.app.utils.betterJSONSerializer import JSONDeserializer, JSONSerializer
-from django.utils import translation
+from arches.app.utils.betterJSONSerializer import JSONSerializer
 from django.views.generic import View
 
 from arches import __version__ as arches_version
 from arches.app.models import models
 from arches.app.utils.response import JSONResponse
 from arches.app.datatypes.datatypes import DataTypeFactory
-
-
-def update_i18n_properties(response):
-    user_language = translation.get_language()
-    config = response["config"]
-
-    if "i18n_properties" in config and isinstance(config["i18n_properties"], list):
-        for prop in config["i18n_properties"]:
-            if (
-                prop in config
-                and isinstance(config[prop], dict)
-                and user_language in config[prop]
-            ):
-                config[prop] = config[prop][user_language]
-    response["config"] = config
-    return response
 
 
 class WidgetDataView(View):
@@ -46,12 +29,7 @@ class WidgetDataView(View):
                 .get()
             )
 
-        response = update_i18n_properties(
-            JSONDeserializer().deserialize(
-                JSONSerializer().serialize(card_x_node_x_widget)
-            )
-        )
-
+        response = JSONSerializer().serialize(card_x_node_x_widget)
         datatype = DataTypeFactory().get_instance(card_x_node_x_widget.node.datatype)
         # When dropping support for v7.6, try/except can be removed
         try:
@@ -80,8 +58,4 @@ class NodeDataView(View):
             )
         )
 
-        response = update_i18n_properties(
-            JSONDeserializer().deserialize(JSONSerializer().serialize(node))
-        )
-
-        return JSONResponse(response)
+        return JSONResponse(JSONSerializer().serialize(node))
