5032a33b78b14da57d23bd8f0de1402377a9c910
brett <brett@qedsystems.ca>
2025-07-08 16:18:53 -0700

    Don't show link in Concept Select Viewer

--- arches_component_lab/src/arches_component_lab/widgets/ConceptSelectWidget/components/ConceptSelectWidgetViewer.vue
+++ arches_component_lab/src/arches_component_lab/widgets/ConceptSelectWidget/components/ConceptSelectWidgetViewer.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import arches from "arches";
 import type { ConceptOption } from "@/arches_component_lab/widgets/types.ts";
 
-const props = defineProps<{
+defineProps<{
     value?: ConceptOption | null | undefined;
 }>();
+
 </script>
 <template>
-    <div :key="props.value?.id">
-        <a :href="`${arches.urls.resource_editor}${props.value?.id}`">
-            {{ props.value?.text }}
-        </a>
+    <div :key="value?.id">
+            {{ value?.text }}
     </div>
 </template>
