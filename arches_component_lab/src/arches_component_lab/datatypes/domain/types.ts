bfc3f14e9c53a04b11b7cd1e0d1d6e5011c7787b
njkim <nkim@fargeo.com>
2025-08-06 18:00:24 -0700

    Add domain value type, #70

---
+++ arches_component_lab/src/arches_component_lab/datatypes/domain/types.ts
@@ -0,0 +1,13 @@
+import type { AliasedNodeData } from "@/arches_component_lab/types.ts";
+
+export interface DomainValue extends AliasedNodeData {
+    display_value: string;
+    node_value: string;
+    details: never[];
+}
+
+export interface DomainOption {
+    id: string;
+    text: string;
+    selected: boolean;
+}
