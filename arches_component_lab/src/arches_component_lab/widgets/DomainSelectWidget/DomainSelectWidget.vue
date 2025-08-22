c67b20c2119755bf1989b0f20cc828c8e59825bb
njkim <nkim@fargeo.com>
2025-08-22 13:41:01 -0700

    Add domain value widget, #70

---
+++ arches_component_lab/src/arches_component_lab/widgets/DomainSelectWidget/DomainSelectWidget.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import DomainSelectWidgetEditor from "@/arches_component_lab/widgets/DomainSelectWidget/components/DomainSelectWidgetEditor.vue";
+import DomainSelectWidgetViewer from "@/arches_component_lab/widgets/DomainSelectWidget/components/DomainSelectWidgetViewer.vue";
+
+import { EDIT, VIEW } from "@/arches_component_lab/widgets/constants.ts";
+
+import type { CardXNodeXWidgetData } from "@/arches_component_lab/types.ts";
+import type { DomainValue } from "@/arches_component_lab/datatypes/domain/types.ts";
+import type { WidgetMode } from "@/arches_component_lab/widgets/types.ts";
+
+defineProps<{
+    mode: WidgetMode;
+    nodeAlias: string;
+    graphSlug: string;
+    cardXNodeXWidgetData: CardXNodeXWidgetData;
+    aliasedNodeData: DomainValue;
+}>();
+
+const emit = defineEmits(["update:isDirty", "update:value"]);
+</script>
+
+<template>
+    <DomainSelectWidgetEditor
+        v-if="mode === EDIT"
+        :card-x-node-x-widget-data="cardXNodeXWidgetData"
+        :graph-slug="graphSlug"
+        :node-alias="nodeAlias"
+        :aliased-node-data="aliasedNodeData"
+        @update:value="emit('update:value', $event)"
+        @update:is-dirty="emit('update:isDirty', $event)"
+    />
+    <DomainSelectWidgetViewer
+        v-if="mode === VIEW"
+        :card-x-node-x-widget-data="cardXNodeXWidgetData"
+        :aliased-node-data="aliasedNodeData"
+    />
+</template>
---
+++ arches_component_lab/src/arches_component_lab/widgets/DomainSelectWidget/components/DomainSelectWidgetEditor.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import Select from "primevue/select";
+
+import type { CardXNodeXWidgetData } from "@/arches_component_lab/types.ts";
+import type { DomainValue, DomainOption } from "@/arches_component_lab/datatypes/domain/types.ts";
+
+const props = defineProps<{
+    cardXNodeXWidgetData: CardXNodeXWidgetData;
+    nodeAlias: string;
+    aliasedNodeData: DomainValue;
+}>();
+
+const options = ref<DomainOption[]>(props.cardXNodeXWidgetData.node.config.options);
+
+const emit = defineEmits<{
+    (event: "update:value", updatedValue: DomainValue): void;
+}>();
+
+function onUpdateModelValue(updatedValue: string | undefined) {
+    if (updatedValue === undefined) {
+        updatedValue = "";
+    }
+
+    emit("update:value", {
+        display_value: updatedValue,
+        node_value: updatedValue,
+        details: [],
+    });
+}
+</script>
+
+<template>
+    <Select
+        option-value="id"
+        option-label="text"
+        :options="options"
+        :placeholder="cardXNodeXWidgetData.config.placeholder"
+        :fluid="true"
+        :model-value="aliasedNodeData.node_value"
+        @update:model-value="onUpdateModelValue($event)"
+    />
+</template>
