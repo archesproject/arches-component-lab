0625f23dc3b650c5d25f987d1f8c0ee3c842250d
njkim <nkim@fargeo.com>
2025-08-22 13:40:44 -0700

    Add domain domain-list type, #70

---
+++ arches_component_lab/src/arches_component_lab/datatypes/domain copy/types.ts
@@ -0,0 +1,19 @@
+import type { AliasedNodeData } from "@/arches_component_lab/types.ts";
+
+export interface DomainValue extends AliasedNodeData {
+    display_value: string;
+    node_value: string;
+    details: never[];
+}
+
+export interface DomainValueList extends AliasedNodeData {
+    display_value: string;
+    node_value: string[];
+    details: never[];
+}
+
+export interface DomainOption {
+    id: string;
+    text: string;
+    selected: boolean;
+}
