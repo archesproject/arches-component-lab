e99044dcb51889772257b614db94c3e716f87bfe
njkim <nkim@fargeo.com>
2025-08-25 21:44:21 -0700

    Initial implementation, #143

---
+++ arches_component_lab/src/arches_component_lab/widgets/DomainRadioWidget/components/DomainRadioWidgetEditor.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import RadioButton from 'primevue/radiobutton';
+
+import type { CardXNodeXWidgetData } from "@/arches_component_lab/types.ts";
+import type { DomainValue, DomainOption } from "@/arches_component_lab/datatypes/domain/types.ts";
+
+const props = defineProps<{
+    cardXNodeXWidgetData: CardXNodeXWidgetData;
+    nodeAlias: string;
+    aliasedNodeData: DomainValue;
+}>();
+
+const options = ref<DomainOption[]>(props.cardXNodeXWidgetData.node.config.options);
+
+const emit = defineEmits<{
+    (event: "update:value", updatedValue: DomainValue): void;
+}>();
+
+function onUpdateModelValue(updatedValue: string | undefined) {
+    if (updatedValue === undefined) {
+        updatedValue = "";
+    }
+
+    emit("update:value", {
+        display_value: updatedValue,
+        node_value: updatedValue,
+        details: [],
+    });
+}
+</script>
+
+<template>
+    <div class="flex flex-wrap gap-4">
+        <div v-for="option in options" :key="option.id">
+            <RadioButton
+                :input-id="option.id"
+                name="dynamic"
+                :model-value="option.id"
+                @update:model-value="onUpdateModelValue($event)"
+            />
+            <label :for="option.id">{{ option.text }}</label>
+        </div>
+    </div>
+</template>
