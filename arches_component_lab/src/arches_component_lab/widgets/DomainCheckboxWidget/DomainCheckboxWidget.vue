ea55536504b1a10ccb588faf7cbfc4f17fdc049a
njkim <nkim@fargeo.com>
2025-08-25 21:44:29 -0700

    Initial implementation, #144

---
+++ arches_component_lab/src/arches_component_lab/widgets/DomainCheckboxWidget/DomainCheckboxWidget.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import DomainCheckboxWidgetEditor from "@/arches_component_lab/widgets/DomainCheckboxWidget/components/DomainCheckboxWidgetEditor.vue";
+import DomainCheckboxWidgetViewer from "@/arches_component_lab/widgets/DomainCheckboxWidget/components/DomainCheckboxWidgetViewer.vue";
+
+import { EDIT, VIEW } from "@/arches_component_lab/widgets/constants.ts";
+
+import type { CardXNodeXWidgetData } from "@/arches_component_lab/types.ts";
+import type { DomainValue } from "@/arches_component_lab/datatypes/domain/types.ts";
+import type { WidgetMode } from "@/arches_component_lab/widgets/types.ts";
+
+defineProps<{
+    mode: WidgetMode;
+    nodeAlias: string;
+    graphSlug: string;
+    cardXNodeXWidgetData: CardXNodeXWidgetData;
+    aliasedNodeData: DomainValue[];
+}>();
+
+const emit = defineEmits(["update:isDirty", "update:value"]);
+</script>
+
+<template>
+    <DomainCheckboxWidgetEditor
+        v-if="mode === EDIT"
+        :card-x-node-x-widget-data="cardXNodeXWidgetData"
+        :graph-slug="graphSlug"
+        :node-alias="nodeAlias"
+        :aliased-node-data="aliasedNodeData"
+        @update:value="emit('update:value', $event)"
+        @update:is-dirty="emit('update:isDirty', $event)"
+    />
+    <DomainCheckboxWidgetViewer
+        v-if="mode === VIEW"
+        :card-x-node-x-widget-data="cardXNodeXWidgetData"
+        :aliased-node-data="aliasedNodeData"
+    />
+</template>
