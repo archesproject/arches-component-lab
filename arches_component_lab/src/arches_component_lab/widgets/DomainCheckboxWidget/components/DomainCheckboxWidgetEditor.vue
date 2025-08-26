9c65171d917c1452f22ff4783d3d2ca8764dd017
njkim <nkim@fargeo.com>
2025-08-26 16:01:19 -0700

    Utilize checkboxgroup, #144

--- arches_component_lab/src/arches_component_lab/widgets/DomainCheckboxWidget/components/DomainCheckboxWidgetEditor.vue
+++ arches_component_lab/src/arches_component_lab/widgets/DomainCheckboxWidget/components/DomainCheckboxWidgetEditor.vue
@@ -2,29 +2,40 @@
 import { ref } from "vue";
 
 import Checkbox from 'primevue/checkbox';
+import CheckboxGroup from 'primevue/checkboxgroup';
 
-import type { CardXNodeXWidgetData } from "@/arches_component_lab/types.ts";
-import type { DomainValue, DomainOption } from "@/arches_component_lab/datatypes/domain/types.ts";
+import type {
+    DomainDatatypeCardXNodeXWidgetData,
+    DomainValueList,
+    DomainOption,
+} from "@/arches_component_lab/datatypes/domain/types.ts";
+import type {  } from "@/arches_component_lab/datatypes/domain/types.ts";
 
-const props = defineProps<{
-    cardXNodeXWidgetData: CardXNodeXWidgetData;
+const { cardXNodeXWidgetData, nodeAlias, aliasedNodeData } = defineProps<{
+    cardXNodeXWidgetData: DomainDatatypeCardXNodeXWidgetData;
     nodeAlias: string;
-    aliasedNodeData: DomainValue;
+    aliasedNodeData: DomainValueList;
 }>();
 
-const options = ref<DomainOption[]>(props.cardXNodeXWidgetData.node.config.options);
+const options = ref<DomainOption[]>(cardXNodeXWidgetData.node.config.options);
 
 const emit = defineEmits<{
-    (event: "update:value", updatedValue: DomainValue): void;
+    (event: "update:value", updatedValue: DomainValueList): void;
 }>();
 
-function onUpdateModelValue(updatedValue: string | undefined) {
-    if (updatedValue === undefined) {
-        updatedValue = "";
-    }
+function onUpdateModelValue(updatedValue: string[] | undefined) {
+    const updatedDisplayValue =
+        updatedValue
+            ?.map((domain) => {
+                const currentOption = options.value.find(
+                    (option: DomainOption) => option.id === domain,
+                );
+                return currentOption?.text;
+            })
+            .join(", ") || "";
 
     emit("update:value", {
-        display_value: updatedValue,
+        display_value: updatedDisplayValue,
         node_value: updatedValue,
         details: [],
     });
@@ -32,15 +43,37 @@ function onUpdateModelValue(updatedValue: string | undefined) {
 </script>
 
 <template>
-    <div class="flex flex-wrap gap-4">
-        <div v-for="option of options" :key="option.id" class="flex items-center gap-2">
+    <CheckboxGroup
+        :model-value="aliasedNodeData.node_value"
+        :name="nodeAlias"
+        class="button-group"
+        @update:model-value="onUpdateModelValue($event)"
+    >
+        <div
+            v-for="option of options"
+            :key="option.id"
+            class="button-group"
+        >
             <Checkbox
                 :input-id="option.id"
-                name="option"
-                :model-value="option.id"
+                :value="option.id"
                 @update:model-value="onUpdateModelValue($event)"
             />
             <label :for="option.id">{{ option.text }}</label>
         </div>
-    </div>
+    </CheckboxGroup>
 </template>
+<style scoped>
+.button-group {
+    display: flex;
+    flex-direction: row;
+    column-gap: 1.5rem;
+    row-gap: 0.5rem;
+    flex-wrap: wrap;
+}
+.checkbox-options {
+    display: flex;
+    gap: 0.25rem;
+    align-items: center;
+}
+</style>
