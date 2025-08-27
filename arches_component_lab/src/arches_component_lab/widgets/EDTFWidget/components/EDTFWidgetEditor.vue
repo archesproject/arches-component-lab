55dae2cfff473a7361380ac9da3d7cfc92eca938
alexwuthrich <awuthrich@fargeo.com>
2025-08-27 13:20:06 -0400

    fixed code style issues

--- arches_component_lab/src/arches_component_lab/widgets/EDTFWidget/components/EDTFWidgetEditor.vue
+++ arches_component_lab/src/arches_component_lab/widgets/EDTFWidget/components/EDTFWidgetEditor.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 import InputText from "primevue/inputtext";
-import Button from 'primevue/button';
-import Drawer from 'primevue/drawer';
+import Button from "primevue/button";
+import Drawer from "primevue/drawer";
 
 import type { CardXNodeXWidgetData } from "@/arches_component_lab/types.ts";
 import type { EdtfValue } from "@/arches_component_lab/datatypes/edtf/types";
@@ -33,51 +33,86 @@ function onUpdateModelValue(updatedValue: string | undefined) {
 
 <template>
     <div class="field-with-button">
-    <InputText
-        class="flex-input"
-        type="text"
-        :fluid="true"
-        :model-value="aliasedNodeData.node_value || ''"
-        :placeholder="cardXNodeXWidgetData.config.placeholder"
-        :pt="{ root: { id: cardXNodeXWidgetData.node.alias } }"
-        @update:model-value="onUpdateModelValue($event)"
+        <InputText
+            class="flex-input"
+            type="text"
+            :fluid="true"
+            :model-value="aliasedNodeData.node_value || ''"
+            :placeholder="cardXNodeXWidgetData.config.placeholder"
+            :pt="{ root: { id: cardXNodeXWidgetData.node.alias } }"
+            @update:model-value="onUpdateModelValue($event)"
         />
-        <Button class="p-button-text custom-text-button" label="EDTF Formatting" @click="visible = true" />
-        <Drawer v-model:visible="visible" position="right" style="height: 100%; width: 50rem" :modal="true" :dissmissable="true">
+        <Button
+            class="p-button-text custom-text-button"
+            label="EDTF Formatting"
+            @click="visible = true"
+        />
+        <Drawer
+            v-model:visible="visible"
+            position="right"
+            style="height: 100%; width: 50rem"
+            :modal="true"
+            :dissmissable="true"
+        >
             <h4>Extended Date/Time Formats (EDTF)</h4>
-            <p>The EDTF datatype allows you to describe dates (even uncertain dates). You can find a summary of the standard here: EDTF Date Specfication (Library of Congress)</p>
+            <p>
+                The EDTF datatype allows you to describe dates (even uncertain
+                dates). You can find a summary of the standard here: EDTF Date
+                Specfication (Library of Congress)
+            </p>
             <p>Some common encodings:</p>
             <p><b>2021-04-12</b> [year][“-”][month][“-”][day]</p>
-            <p>Refers to the calendar date 2021 April 12th with day precision</p>
+            <p>
+                Refers to the calendar date 2021 April 12th with day precision
+            </p>
             <p><b>2021-04</b> [year][“-”][month]</p>
             <p>Refers to the calendar month April 2021 with month precision</p>
             <p><b>2021</b> [year]</p>
             <p>Refers to the year 2021 with year precision</p>
             <p><b>1964/2008</b></p>
-            <p>A time interval with calendar year precision, beginning sometime in 1964 and ending sometime in 2008</p>
+            <p>
+                A time interval with calendar year precision, beginning sometime
+                in 1964 and ending sometime in 2008
+            </p>
             <p><b>2004-06/2006-08</b></p>
-            <p>A time interval with calendar month precision, beginning sometime in June 2004 and ending sometime in August of 2006</p>
+            <p>
+                A time interval with calendar month precision, beginning
+                sometime in June 2004 and ending sometime in August of 2006
+            </p>
             <p><b>2004-02-01/2005</b></p>
-            <p>A time interval with calendar year precision, beginning sometime in 1964 and ending sometime in 2008</p>
+            <p>
+                A time interval with calendar year precision, beginning sometime
+                in 1964 and ending sometime in 2008
+            </p>
             <p><b>Y-100000</b></p>
-            <p>The year -100000. 'Y' may be used at the beginning of the date string to signify that the date is a year, when (and only when) the year exceeds four digits, i.e. for years later than 9999 or earlier than -9999.</p>
+            <p>
+                The year -100000. 'Y' may be used at the beginning of the date
+                string to signify that the date is a year, when (and only when)
+                the year exceeds four digits, i.e. for years later than 9999 or
+                earlier than -9999.
+            </p>
             <p><b>2001-21</b></p>
-            <p>Spring, 2001. The values 21, 22, 23, 24 may be used used to signify ' Spring', 'Summer', 'Autumn', 'Winter', respectively, in place of a month value (01 through 12) for a year-and-month format string</p>
+            <p>
+                Spring, 2001. The values 21, 22, 23, 24 may be used used to
+                signify ' Spring', 'Summer', 'Autumn', 'Winter', respectively,
+                in place of a month value (01 through 12) for a year-and-month
+                format string
+            </p>
             <p><b>1984?</b></p>
             <p>Year uncertain (possibly the year 1984, but not definitely)</p>
             <p><b>2004-06~</b></p>
             <p><b>2004-06-11%</b></p>
             <p>Entire date (year-month-day) uncertain and approximate</p>
         </Drawer>
-        </div>
+    </div>
 </template>
 
 <style scoped>
 .field-with-button {
-  display: flex;
-  align-items: center;
-  gap: 0.5rem; /* space between input and button */
-  width: 100%;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem; /* space between input and button */
+    width: 100%;
 }
 
 .flex-input {
@@ -86,11 +121,11 @@ function onUpdateModelValue(updatedValue: string | undefined) {
 }
 
 .custom-text-button {
-  font-weight: bold;
-  background-color: transparent !important;
-  border: none !important;
-  box-shadow: none !important;
-  padding: 0rem 2rem;
-  white-space: nowrap;
+    font-weight: bold;
+    background-color: transparent !important;
+    border: none !important;
+    box-shadow: none !important;
+    padding: 0rem 2rem;
+    white-space: nowrap;
 }
 </style>
