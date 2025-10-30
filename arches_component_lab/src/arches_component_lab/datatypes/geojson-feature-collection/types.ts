cae47b112df0c20f6e1da29aac7b379765d62f94
brett <brett@qedsystems.ca>
2025-10-30 10:37:20 -0700

    Use standard @types/geojson types

--- arches_component_lab/src/arches_component_lab/datatypes/geojson-feature-collection/types.ts
+++ arches_component_lab/src/arches_component_lab/datatypes/geojson-feature-collection/types.ts
@@ -1,35 +1,8 @@
 import type { AliasedNodeData } from "@/arches_component_lab/types.ts";
-
-export interface PointGeometry {
-    type: "Point";
-    coordinates: [number, number];
-}
-
-export interface LineGeometry {
-    type: "Line";
-    coordinates: [[number, number], [number, number]];
-}
-
-export interface PolygonGeometry {
-    type: "Polygon";
-    coordinates: [number, number][];
-}
-
-export interface GeoJSONFeature {
-    id: string;
-    type: "Feature";
-    geometry: PointGeometry | LineGeometry | PolygonGeometry;
-    properties: Record<string, unknown>;
-}
-
-export interface GeoJSONFeatureCollection {
-    type: 'FeatureCollection';
-    features: GeoJSONFeature[];
-    properties: Record<string, unknown>;
-}
+import type { FeatureCollection } from "geojson";
 
 export interface GeoJSONFeatureCollectionValue extends AliasedNodeData {
     display_value: string;
-    node_value: GeoJSONFeatureCollection | null;
+    node_value: FeatureCollection | null;
     details: never[];
 }
