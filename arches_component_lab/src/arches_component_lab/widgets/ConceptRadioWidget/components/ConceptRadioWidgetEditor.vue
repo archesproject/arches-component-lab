f3f3d58e3f1539614c13bf001f86a8fa3582ab76
brett <brett@qedsystems.ca>
2025-11-19 17:06:52 -0800

    Fix ConceptRadioWidgetEditor namespace collision.

--- arches_component_lab/src/arches_component_lab/widgets/ConceptRadioWidget/components/ConceptRadioWidgetEditor.vue
+++ arches_component_lab/src/arches_component_lab/widgets/ConceptRadioWidget/components/ConceptRadioWidgetEditor.vue
@@ -79,7 +79,7 @@ function onUpdateModelValue(selectedOption: Record<string, boolean> | null) {
 <template>
     <RadioButtonGroup
         :model-value="selectedId"
-        :name="nodeAlias"
+        :name="nodeAlias + '__ui'"
         :class="['button-group', flexDirection]"
         @update:model-value="onUpdateModelValue"
     >
