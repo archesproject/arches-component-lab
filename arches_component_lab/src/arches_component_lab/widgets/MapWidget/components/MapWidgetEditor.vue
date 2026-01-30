7522059ab0620175c9cd07b44616d95cf827a137
Alexei Peters <apeters@fargeo.com>
2026-01-29 16:15:30 -0800

    add stub out of Map widget

---
+++ arches_component_lab/src/arches_component_lab/widgets/MapWidget/components/MapWidgetEditor.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { GeoJSONFeatureCollectionValue } from "@/arches_component_lab/datatypes/geojson-feature-collection/types";
+
+defineProps<{
+    aliasedNodeData: GeoJSONFeatureCollectionValue | null;
+}>();
+</script>
+
+<template>
+    <input
+        disabled
+        :value="aliasedNodeData?.display_value"
+    />
+</template>
