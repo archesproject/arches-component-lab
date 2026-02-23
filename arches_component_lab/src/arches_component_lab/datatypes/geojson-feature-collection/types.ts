23f44ad559174044d19420206cb6627ba6abdb52
Philip Hansen <philip.pt.hansen@gmail.com>
2026-02-23 15:59:28 -0700

    geojson bug fix

--- arches_component_lab/src/arches_component_lab/datatypes/geojson-feature-collection/types.ts
+++ arches_component_lab/src/arches_component_lab/datatypes/geojson-feature-collection/types.ts
@@ -1,8 +1,7 @@
 import type { AliasedNodeData } from "@/arches_component_lab/types.ts";
-import type { FeatureCollection } from "geojson";
 
 export interface GeoJSONFeatureCollectionValue extends AliasedNodeData {
     display_value: string;
-    node_value: FeatureCollection | null;
+    node_value: GeoJSON.FeatureCollection | null;
     details: never[];
 }
