1b00fe45dacad62bda1bb7e8710374353a7209d2
Rob Gaston <rgaston@fargeo.com>
2026-02-27 13:31:03 -0800

    fixes version number comparisons

--- arches_component_lab/views/api/relatable_resources.py
+++ arches_component_lab/views/api/relatable_resources.py
@@ -5,7 +5,9 @@ from django.utils.translation import get_language
 from arches.app.models.models import Node, ResourceInstance, GraphModel
 from arches.app.utils.response import JSONResponse
 from arches.app.utils.betterJSONSerializer import JSONDeserializer
-from arches import __version__ as arches_version
+from arches import __version__ as _arches_version_str
+from packaging.version import Version
+arches_version = Version(_arches_version_str)
 from django.db.models import Q, F
 
 
@@ -16,7 +18,7 @@ class RelatableResourcesView(View):
             graph__slug=graph,
             graph__publication__isnull=False,
         )
-        if arches_version >= (8, 0):
+        if arches_version >= Version("8.0"):
             node_filter = node_filter & Q(graph__is_active=True)
 
         node = Node.objects.get(node_filter)
